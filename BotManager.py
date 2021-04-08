5da096b88b5e9749579ed560cf43a4077369dbf2
Magno de Oliveira Paz <magno01@gmail.com.br>
2021-04-07 23:40:34 -0300

    Base functions

---
+++ BotManager.py
@@ -0,0 +1,4 @@
+import Settings
+from Bots import Manager
+
+Manager.start_bot()
---
+++ Bots/Manager.py
@@ -0,0 +1,34 @@
+def start_bot():
+    print('Deploying a second manager for backup on sleep mode.')
+    deploy_bot('Manager', 'sleep')
+    return
+
+
+def deploy_bot(bot, args):
+    print(F'deploying {bot}')
+    return
+
+
+def check_manager():
+    print('checking for another manager')
+    return
+
+
+def keep_alive_manager():
+    print('send signal to backup manager')
+    return
+
+
+def check_system_cpu():
+    print('checking system CPU')
+    return
+
+
+def kill_process(id):
+    print(f'Killing {id} process')
+    return
+
+
+def check_high_demanding_process():
+    print('Checking for a high demanding process')
+    return
